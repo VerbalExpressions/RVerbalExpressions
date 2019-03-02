9927a69ce1caaff6f4f927aab03b52951aeb57e0
tylurp <tylurp1@gmail.com>
2019-03-02 10:19:23 -0800

    update docs

--- R/anything_but.R
+++ R/anything_but.R
@@ -1,16 +1,23 @@
 #' Match any character(s) except these any (including zero) number of times.
 #'
-#' @details This function is slighly different than \code{start_of_line()}
-#' because it has the additional argument \code{.data} which allows the user to
-#' pass a value to to it. This is meant to allow \code{dplyr} chains to
-#' function properly as \code{.data} defaults to \code{NULL} and will pull
-#' the data from the previous step in the chain.
+#' @description This expression will match everything except whatever characters
+#' the user specifies in the \code{value} parameter. It does this by adding a
+#' caret symbol \code{^} at the beginning of a character set \code{[]}. Typing
+#' a caret after the opening square bracket negates the character class. The
+#' result is that the character class matches any character that is not in the
+#' character class. Unlike the dot, negated character classes also match
+#' (invisible) line break characters. If you don't want a negated character
+#' class to match line breaks, you need to include the line break characters in
+#' the class.
 #'
 #' @param .data Expression to append, typically pulled from the pipe \code{ \%>\% }
 #' @param value Characters to not match
 #'
 #' @examples
 #' anything_but(value = "abc")
+#'
+#' @references
+#' Character Class: \url{https://www.regular-expressions.info/charclass.html}
 #' @export
 anything_but <- function(.data = NULL, value) {
   paste0(.data, "(?:[^", sanitize(value), "]*)")
--- R/br.R
+++ R/br.R
@@ -1,11 +1,7 @@
 #' Match a line break (alias of \code{line_break()}).
 #'
-#' @details This function is looks for line breaks with the following
-#' expression: \code{(?:\\r\\n|\\r|\\n)}
-#' \enumerate{
-#'   \item Unix style: \url{https://codepoints.net/U+000A}
-#'   \item Windows style: \url{https://codepoints.net/U+000D}
-#' }
+#' @description This expression looks for line breaks, both Unix and Windows
+#' style by using the appropriate \emph{non printable characters}.
 #'
 #' @param .data Expression to append, typically pulled from the pipe \code{ \%>\% }
 #'
@@ -20,6 +16,13 @@
 #'
 #' # extract match
 #' regmatches(string, regexpr(x, string))
+#'
+#' @references
+#' Unix style: \url{https://codepoints.net/U+000A}
+#'
+#' Windows style: \url{https://codepoints.net/U+000D}
+#'
+#' Non printable character: \url{https://www.regular-expressions.info/nonprint.html}
 #' @export
 br <- function(.data = NULL) {
   paste0(.data, "(?:\\r\\n|\\r|\\n)")
