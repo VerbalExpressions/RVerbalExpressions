a31f1994060360306845936e3a7d18ca4a8a1bb4
tylurp <tylurp1@gmail.com>
2019-03-02 07:49:59 -0800

    add test for anything_but rule

--- tests/testthat/test-anything_but.R
+++ tests/testthat/test-anything_but.R
@@ -1,5 +1,12 @@
 context("test-anything_but")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("anything_but rule works", {
+  # exoect the correct output
+  expect_equal(anything_but(value = "abc"), "(?:[^abc]*)")
+
+  # expect an error if nothing is given
+  expect_error(anything_but())
+
+  # expect .data works
+  expect_equal(anything_but("^", value = "abc"), "^(?:[^abc]*)")
 })
