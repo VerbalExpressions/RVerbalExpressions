08c042091f39f61dde6a8ec89ae67f605855f7ca
tylurp <tylurp1@gmail.com>
2019-03-02 11:32:48 -0800

    update docs

--- R/something_but.R
+++ R/something_but.R
@@ -1,10 +1,9 @@
 #' Match any character(s) except these at least once.
 #'
-#' @details This function is slighly different than \code{start_of_line()}
-#' because it has the additional argument \code{.data} which allows the user to
-#' pass a value to to it. This is meant to allow \code{dplyr} chains to
-#' function properly as \code{.data} defaults to \code{NULL} and will pull
-#' the data from the previous step in the chain.
+#' @description This expression is almost identical to \code{anything_but()}
+#' with one major exception, a \code{+} is used instead of a \code{*}. This
+#' means \code{something_but()} expects \emph{something} whereas
+#' \code{anything_but()} expects \emph{anything} including... nothing!
 #'
 #' @param .data Expression to append, typically pulled from the pipe \code{ \%>\% }
 #' @param value Expression to optionally match
@@ -17,6 +16,9 @@
 #'
 #' grepl(x, "R")  # should be true
 #' grepl(x, "py") # should be false
+#'
+#' @references
+#' Metacharacters: \url{https://www.regular-expressions.info/characters.html#special}
 #' @export
 something_but <- function(.data = NULL, value) {
   paste0(.data, "(?:[^", sanitize(value), "]+)")
