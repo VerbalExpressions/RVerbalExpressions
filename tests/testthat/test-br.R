d4e43ebb572b8301d8de0fdf0dd2ab651293a2da
tylurp <tylurp1@gmail.com>
2019-03-03 11:42:25 -0800

    another test for `br`

---
+++ tests/testthat/test-br.R
@@ -0,0 +1,19 @@
+context("test-br")
+
+test_that("br special character works", {
+
+  # expect minimal output
+  expect_equal(br(), "(?:\\r\\n|\\r|\\n)")
+
+  # expect match
+  expect_true(grepl(br(), "\n"))
+
+  # expect match
+  expect_true(grepl(br(), "\r"))
+
+  # expect match
+  expect_true(grepl(br(), "hello\nworld"))
+
+  # expect match
+  expect_true(grepl(br(), "hello\rworld"))
+})
