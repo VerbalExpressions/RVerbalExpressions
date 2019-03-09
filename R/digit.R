f2c71bcbc95f55e7e9dd71717566a1ac443ea8e3
tylurp <tylurp1@gmail.com>
2019-03-09 08:15:22 -0800

    adjust `rx_digit` docs so description shows up

--- R/digit.R
+++ R/digit.R
@@ -1,8 +1,9 @@
 #' Match a digit (0–9).
 #'
-#' @details The function \code{rx_digit()}looks for tabs with the following expression:
-#' \code{\\d} and matches single digit. Plural version matches specified number of digits \code{n}
-#' (equivalent to \code{rx_digit() %>% rx_count(n)}).
+#' @description The function \code{rx_digit()}looks for tabs with the following
+#' expression: \code{\%\%d} and matches single digit. Plural version matches
+#' specified number of digits \code{n} (equivalent to \code{rx_digit() \%>\% rx_count(n)}).
+#'
 #' @rdname rx_digit
 #' @param .data Expression to append, typically pulled from the pipe \code{ \%>\% }
 #'
