9f53f976432cb40fc82b12eee90619a9e55ef93e
tylurp <tylurp1@gmail.com>
2019-03-11 08:25:59 -0700

    inverse to upper/lower case

--- R/lowercase.R
+++ R/lowercase.R
@@ -3,19 +3,27 @@
 #' @description Matches lower case letters only.
 #'
 #' @param .data Expression to append, typically pulled from the pipe \code{ \%>\% }
+#' @param inverse Invert match behavior, defaults to \code{FALSE} (match
+#' lower case). Use \code{FALSE} to \emph{not} match lower case.
 #'
 #' @examples
 #' rx_lowercase()
+#' rx_lowercase(inverse = TRUE)
 #'
 #' # create an expression
 #' x <- rx_lowercase()
+#' y <- rx_lowercase(inverse = TRUE)
 #'
 #' # create input
 #' string <- "Apple 1!"
 #'
 #' # extract match
 #' regmatches(string, gregexpr(x, string))
+#' regmatches(string, gregexpr(y, string))
 #' @export
-rx_lowercase <- function(.data = NULL) {
-  paste0(.data, "[[:lower:]]")
+rx_lowercase <- function(.data = NULL, inverse = FALSE) {
+  switch (as.character(inverse),
+    "FALSE" = paste0(.data, "[[:lower:]]"),
+    "TRUE" = paste0(.data, "[^[:lower:]]")
+  )
 }
--- R/uppercase.R
+++ R/uppercase.R
@@ -3,19 +3,27 @@
 #' @description Matches upper case letters only.
 #'
 #' @param .data Expression to append, typically pulled from the pipe \code{ \%>\% }
+#' @param inverse Invert match behavior, defaults to \code{FALSE} (match
+#' upper case). Use \code{FALSE} to \emph{not} match upper case.
 #'
 #' @examples
 #' rx_uppercase()
+#' rx_uppercase(inverse = TRUE)
 #'
 #' # create an expression
 #' x <- rx_uppercase()
+#' y <- rx_uppercase(inverse = TRUE)
 #'
 #' # create input
 #' string <- "Apple 1!"
 #'
 #' # extract match
 #' regmatches(string, gregexpr(x, string))
+#' regmatches(string, gregexpr(y, string))
 #' @export
-rx_uppercase <- function(.data = NULL) {
-  paste0(.data, "[[:upper:]]")
+rx_uppercase <- function(.data = NULL, inverse = FALSE) {
+  switch (as.character(inverse),
+    "FALSE" = paste0(.data, "[[:upper:]]"),
+    "TRUE" = paste0(.data, "[^[:upper:]]")
+  )
 }
